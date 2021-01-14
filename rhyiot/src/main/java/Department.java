490a82cfbe039d961d071f15b3460005fee60cd5
chenxl <censinli@126.com>
2021-01-14 21:47:34 +0800

    RHYIOT GIT DESCRIPTION 新增属性

---
+++ rhyiot/src/main/java/Department.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package main.java;/*
+ *@Author:CENSINLI
+ *@DATE 2021-01-14
+ *@Description smartiot main.java
+ *@Version V1.0
+ */
+
+import java.security.PublicKey;
+
+public class Department {
+    public static void main(String[] args) {
+        String deptname ="caiwubu";
+        String deptno = "00001";
+    }
+}
